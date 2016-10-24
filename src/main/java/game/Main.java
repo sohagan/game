a7c09130de03c140417cae2ce32499039ee3b9c7
sohagan <shaun@codebran.com>
2016-10-24 19:02:13 +0100

    code reformtted

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,9 +1,6 @@
-
 public class Main {
 
-public static void main(String args[]) {
-
-	System.out.println("Game is running");
-}
-
+	public static void main(String args[]) {
+		System.out.println("Game is running");
+	}
 }
